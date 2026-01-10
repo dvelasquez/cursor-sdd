f05e668d0d6a73f10e097dd6f2a91ad6e416bc9b
Danilo Velasquez <danilo.velasquez@gmail.com>
2026-01-10 10:05:52 +0100

    astro install

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
